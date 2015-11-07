05acfa2969cc9e7dd696a47cd17f1862b01f8fb9
Brandon DeWitt <brandondewitt1@gmail.com>
2015-11-06 21:41:40 -0800

    return better responses from api

--- api/pins.js
+++ api/pins.js
@@ -11,12 +11,16 @@ app.post('/pins', (req, res) => {
   pg.connect(connectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).send(json({ success: false, data: err}));
+      return res.status(500).json({ success: false, data: err});
     }
 
-    client.query("INSERT INTO pins(image, description) values($1, $2)", [pin.image, pin.description]);
-    done();
-    res.json(req.body);
+    client.query("INSERT INTO pins(image, description) values($1, $2) returning id", [pin.image, pin.description], (err, result) => {
+      done();
+      if (err) {
+        return res.status(500).json({ success: false, data: err});
+      }
+      res.status(201).json(result.rows[0]);
+    });
   });
 });
 
@@ -26,31 +30,34 @@ app.put('/pins/:id', (req, res) => {
   pg.connect(connectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).send(json({ success: false, data: err}));
+      return res.status(500).json({ success: false, data: err});
     }
 
-    client.query("UPDATE pins SET image=($1), description=($2) WHERE id=($3)", [pin.image, pin.description, id]);
-    done();
-    res.json(req.body);
+    client.query("UPDATE pins SET image=($1), description=($2) WHERE id=($3)", [pin.image, pin.description, id], (err, result) => {
+      done();
+      if (err) {
+        return res.status(500).json({ success: false, data: err});
+      }
+      res.status(204).json(pin);
+    });
   });
 });
 
 app.delete('/pins/:id', (req, res) => {
   const id = req.params.id;
-  let results = [];
   pg.connect(connectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).send(json({ success: false, data: err}));
+      return res.status(500).json({ success: false, data: err});
     }
 
-    client.query("DELETE FROM pins WHERE id=($1)", [id]);
-    let query = client.query('SELECT id, image, description FROM pins');
-    query.on('row', row => results.push(row));
-    query.on('end', () => {
+    client.query("DELETE FROM pins WHERE id=($1)", [id], (err) => {
       done();
-      return res.json(results);
-    })
+      if (err) {
+        return res.status(500).json({ success: false, data: err});
+      }
+      res.status(204).send();
+    });
   });
 });
 
@@ -59,31 +66,33 @@ app.get('/pins/:id', (req, res) => {
   pg.connect(connectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).send(json({ success: false, data: err}));
+      return res.status(500).json({ success: false, data: err});
     }
 
-    let query = client.query('SELECT id, image, description FROM pins WHERE id=($1)', [id]);
-    query.on('row', row => {
+    client.query('SELECT id, image, description FROM pins WHERE id=($1)', [id], (err, result) => {
       done();
-      return res.json(row);
-    })
+      if (err) {
+        return res.status(500).json({ success: false, data: err});
+      }
+      return res.json(result.rows[0]);
+    });
   });
 });
 
 app.get('/pins', (req, res) => {
-  let results = [];
   pg.connect(connectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).send(json({ success: false, data: err}));
+      return res.status(500).json({ success: false, data: err});
     }
 
-    let query = client.query('SELECT id, image, description FROM pins');
-    query.on('row', row => results.push(row));
-    query.on('end', () => {
+    let query = client.query('SELECT id, image, description FROM pins', (err, result) => {
       done();
-      return res.json(results);
-    })
+      if (err) {
+        return res.status(500).json({ success: false, data: err});
+      }
+      res.json(result.rows);
+    });
   });
 });
 
